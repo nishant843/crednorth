e9dfc92ff656f798870dbed61acd0e2433461918
nishant843 <nishant@crednorth.com>
2026-05-10 21:20:36 +0530

    Add files via upload

--- backend/celery.py
+++ backend/celery.py
@@ -1,9 +1,17 @@
 import os
-
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-app = Celery('backend')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("backend")
+
+# Load settings from Django settings.py
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Explicitly ensure broker/backend loaded
+app.conf.broker_url = os.getenv("REDIS_URL")
+app.conf.result_backend = os.getenv("REDIS_URL")
+
 app.autodiscover_tasks()
+
+print("CELERY BROKER:", app.conf.broker_url)
